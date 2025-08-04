68acba17d96e7c72582d859b5f4b5a8f3be69afa
Prudhvi2206 <prudhviraj200518@gmail.com>
2025-08-04 19:53:49 +0530

    Add files via upload

---
+++ HelloPrudhvi.java
@@ -0,0 +1,11 @@
+public class HelloPrudhvi {
+    public static void main(String[] args) {
+        System.out.println("Hello, Raj!");
+        
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
